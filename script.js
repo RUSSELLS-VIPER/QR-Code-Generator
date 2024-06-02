e91802e7048add66f113f4a4a6d0478ef49b0659
ARIJIT <144230345+RUSSELLS-VIPER@users.noreply.github.com>
2024-06-03 02:36:35 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
-let downloadBtn = document.getElementById("downloadBtn");
 
 function generateQR() {
     if (qrText.value.length > 0) {
         let qrURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
         qrImage.src = qrURL;
         imgBox.classList.add("show-img");
-        
-        // Show the download button
-        downloadBtn.style.display = 'block';
+
+        // Automatically trigger download
+        downloadQR(qrURL);
     } else {
         qrText.classList.add('error');
         setTimeout(() => {
@@ -19,8 +18,7 @@ function generateQR() {
     }
 }
 
-function downloadQR() {
-    let qrURL = qrImage.src;
+function downloadQR(qrURL) {
     let link = document.createElement('a');
     link.href = qrURL;
     link.download = 'qrcode.png';
